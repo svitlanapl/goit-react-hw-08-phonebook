87beb0800d84dab674899ea95a3e7acdff5f2ca3
svitlanapl <sveta_pr@i.ua>
2022-12-25 13:35:28 +0200

    fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,38 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/ContactForm';
 import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/Filter';
 
-export const App= () => {
-  const [contacts, setContacts] = useState([
+const useLocalStorage = (contacts, defaultValue) => {
+  const [localState, setLocalState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(contacts)) ?? defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(contacts, JSON.stringify(localState));
+  }, [contacts, localState]);
+
+  return [localState, setLocalState];
+};
+
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('defaultValue', [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
   const [filter, setFilter] = useState('');
-  const isFirstRender = useRef(true);
-
-  useEffect(() => {
-    console.log('Hello, this is useEffect' + Date.now());
-    const savedContacts = localStorage.getItem('contacts');
-    savedContacts !== null
-      ? setContacts(JSON.parse(savedContacts))
-      : setContacts('contacts')
-  }, []);
-
-  useEffect(() => { 
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const onChangeFilter = e => {
-        setFilter(e.target.value.toLowerCase());
-  };
 
   const addNewContact = ({ name, number }) => { 
     const searchName = name.toLowerCase().trim();
@@ -52,13 +42,19 @@ export const App= () => {
 
     setContacts(prevContacts => [...prevContacts, newContact]);
   };
-
-    const getFilteredContacts = () => {
+  
+  const onChangeFilter = e => {
+    setFilter(e.target.value.toLowerCase());
+  };
+  
+  const getFilteredContacts = () => {
     return contacts.filter(
       contact => contact.name.toLowerCase().includes(filter)
     )
   };
-
+  
+  // localStorage.clear();
+  
   const deleteContact = id => {
     setContacts(prevContacts =>
       prevContacts.filter(contact => contact.id !== id)
@@ -66,26 +62,24 @@ export const App= () => {
   };
 
   return (
-          <div>
-            <h1>Phonebook</h1>
-            <ContactForm
-              addNewContact={addNewContact}
-            />
-            <h2>Contacts</h2>
-            <Filter
-              onChange={onChangeFilter}
-              value={filter}
-            />
-            <ContactList
-              filteredContacts={getFilteredContacts()}
-              deleteContact={deleteContact}
-            />
-          </div>
-        );
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm
+        addNewContact={addNewContact}
+        />
+      <h2>Contacts</h2>
+      <Filter
+        onChange={onChangeFilter}
+        value={filter}
+      />
+      <ContactList
+        filteredContacts={getFilteredContacts()}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
 };
 
-
- 
 
 
 
