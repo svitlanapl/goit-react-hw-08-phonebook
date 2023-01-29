4b09d0b4419ae024e03ac670c040d8f688641f67
svitlanapl <sveta_pr@i.ua>
2023-01-29 12:24:08 +0200

    fixes refresh

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
@@ -59,6 +60,73 @@ export const App = () => {
   );
 
 };
+
+
+
+
+// =======================================================
+
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Route, Routes } from 'react-router-dom';
+// import { Layout } from 'components/Layout/Layout';
+// import { PrivateRoute } from 'components/PrivateRoute';
+// import { RestrictedRoute } from 'components/RestrictedRoute';
+// import { refreshUser } from 'redux/auth/operations';
+// import { useAuth } from 'hooks/useAuth';
+
+// import { HomePage } from 'pages/Home';
+// import { RegisterPage } from 'pages/Register';
+// import { LoginPage } from 'pages/Login';
+// import { ContactsPage } from 'pages/Contacts';
+
+// export const App = () => {
+//   const dispatch = useDispatch();
+//   const { isRefreshing } = useAuth();
+
+//   useEffect(() => {
+//     dispatch(refreshUser());
+//   }, [dispatch]);
+
+//   return isRefreshing ? (
+//     <b>Refreshing user...</b>
+//   ) : (
+//     <Routes>
+//       <Route path="/" element={<Layout />}>
+//         <Route index element={<HomePage />} />
+//         <Route
+//           path="/register"
+//           element={
+//             <RestrictedRoute
+//               redirectTo="/contacts"
+//               component={<RegisterPage />}
+//             />
+//           }
+//         />
+//         <Route
+//           path="/login"
+//           element={
+//             <RestrictedRoute
+//               redirectTo="/contacts"
+//               component={<LoginPage />}
+//             />
+//           }
+//         />
+//         <Route
+//           path="/contacts"
+//           element={
+//             <PrivateRoute
+//               redirectTo="/login"
+//               component={<ContactsPage />}
+//             />
+//           }
+//         />
+//       </Route>
+//     </Routes>
+
+//   );
+
+// };
 
 
 
